2f84313c6629ac7ab2fbb16407041ce928535635
midoI <ahmed.jardak@gmail.com>
2013-09-30 11:09:31 +0100

    add readTotalFileFunctions(); add modifyFilesOperations

--- org/home/files/ModifyFilesOperations.php
+++ org/home/files/ModifyFilesOperations.php
@@ -10,4 +10,23 @@
 class ModifyFilesOperations
 {
 
+
+    function __construct()
+    {
+        echo copy("monfichier.txt", "monfichier.back");
+        echo PHP_EOL . "------------------------------" . PHP_EOL;
+        echo rename("monfichier.back", "monfichier.back.txt");
+        echo PHP_EOL . "------------------------------" . PHP_EOL;
+        if (file_exists("monfichier.back.txt")) {
+            $result = (unlink("monfichier.back.txt")) ? "Suppression réalisée" : "Echec de
+➥la suppression";
+        } else {
+            echo "Le fichier n'existe pas dans ce répertoire! <br>";
+        }
+        echo $result;
+        echo PHP_EOL . "------------------------------" . PHP_EOL;
+        echo filetype("monfichier.txt");
+    }
 }
+
+new ModifyFilesOperations();
