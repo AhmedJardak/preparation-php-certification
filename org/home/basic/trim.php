b30b5ab967e242f4183cd85c41152e75bd4c4d1d
midoI <ahmed.jardak@gmail.com>
2013-10-25 16:31:05 +0100

    add trim example

---
+++ org/home/basic/trim.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: ajar
+ * Date: 25/10/13
+ * Time: 15:41
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+$sample = "$/fffgfgfgfgf/$";
+
+echo rtrim($sample, "$/") . PHP_EOL;
+
+echo ltrim($sample, "$/") . PHP_EOL;
+
+echo trim($sample, "$/") . PHP_EOL;
+
